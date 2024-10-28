593b7a669b053327b9575bdd5122310bae5c07f1
Sergio <estrada.serg@gmail.com>
2024-10-28 14:52:08 -0500

    created item class

---
+++ javaGameButCSharp/src/Models/Item.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace JavaGameButCSharp{
+    class Item{
+        private string name {get;}
+        private string description {get;}
+        private int modifier {get;}
+        private int condition {get;}
+        private int value {get;}
+        private string type{get;}
+
+        Item(){
+
+        }
+    }
+}
