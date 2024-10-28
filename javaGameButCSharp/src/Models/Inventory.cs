ab4c0f12df9808b3b44a8d1c4cedd870a6986897
Sergio <estrada.serg@gmail.com>
2024-10-28 14:51:41 -0500

    created inventory class

---
+++ javaGameButCSharp/src/Models/Inventory.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace JavaGameButCSharp{
+    class Inventory{
+        private string activeWeapon {get;}
+        private string activeArmor {get;}
+
+        public Inventory(){
+
+        }
+    }
+
+
+}
