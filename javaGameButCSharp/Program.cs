1b2b9521d16ef43871a2ba574b624bdbec5eebe7
Sergio <estrada.serg@gmail.com>
2024-10-28 14:50:30 -0500

    main program

---
+++ javaGameButCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JavaGameButCSharp{
+    public class MainGame{
+        static void Main(){
+            string test = "hello";
+
+            if(test.Equals("hello")){
+                Console.WriteLine("world");
+            }
+        }
+    }
+}
