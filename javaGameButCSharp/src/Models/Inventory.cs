bf8ff5f951120dd5294859a5f490b05817e09963
Sergio <estrada.serg@gmail.com>
2024-10-30 07:26:59 -0500

    added json annotations and more class properties

--- javaGameButCSharp/src/Models/Inventory.cs
+++ javaGameButCSharp/src/Models/Inventory.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace JavaGameButCSharp{
     class Inventory{
-        private string activeWeapon {get;}
-        private string activeArmor {get;}
+        [JsonPropertyName("activeWeapon")]
+        public string ActiveWeapon { get; set; }
+
+        [JsonPropertyName("activeArmor")]
+        public string ActiveArmor { get; set; }
+
+        [JsonPropertyName("inventory")]
+        public List<Item> EntityInventory {get; set;}
 
         public Inventory(){
-
+            this.ActiveArmor = String.Empty;
+            this.ActiveWeapon = String.Empty;
+            this.EntityInventory = [];
         }
     }
 
