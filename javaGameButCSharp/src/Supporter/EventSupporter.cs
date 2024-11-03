3df7044329a77b03b9f5977ade14c2ad0adaebc8
Sergio <estrada.serg@gmail.com>
2024-11-02 22:37:00 -0500

    added new attributes

--- javaGameButCSharp/src/Supporter/EventSupporter.cs
+++ javaGameButCSharp/src/Supporter/EventSupporter.cs
@@ -1,5 +1,7 @@
 namespace JavaGameButCSharp{
     class EventSupporter(){
+        public EventModel NextEvent {get;}
+        public OptionMap EventOutCome {get;}
         public EventModel RunEvent(Event nextEvent){
             return new EventModel(OptionMap.REPEAT, OptionMap.REPEAT, "TEST");
         }
