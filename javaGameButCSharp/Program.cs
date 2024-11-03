a0010e7451d165d5071e45bd319107e3e2f70052
Estrada <sergio.estrada@accenture.com>
2024-11-02 22:08:44 -0500

    program.cs

--- javaGameButCSharp/Program.cs
+++ javaGameButCSharp/Program.cs
@@ -4,15 +4,9 @@ using static JavaGameButCSharp.OptionMap;
 namespace JavaGameButCSharp{
     public class MainGame{
         static void Main(){
-            SaveLoadManagement saveLoad = new SaveLoadManagement();
-            JsonStateManagement jsonManager = new JsonStateManagement();
+            Engine gameEngine = new Engine();
 
-            saveLoad.NewSave("NEWGAME");
-
-            string userLoadPath = saveLoad.GetStatePath(ENTITY, "PLAYER");
-            Entity player = jsonManager.Read<Entity>(userLoadPath);
-
-            Console.WriteLine(player.Inventory.EntityInventory[0].Type);
+            gameEngine.NewPlayer("MyNewGame");
         }
     }
 }
