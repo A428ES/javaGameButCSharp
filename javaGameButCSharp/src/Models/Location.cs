d4b5eecdcb20ebe4878dae52558e9d9d5d04f720
Sergio <estrada.serg@gmail.com>
2024-11-04 09:18:23 -0600

    added location attributes

--- javaGameButCSharp/src/Models/Location.cs
+++ javaGameButCSharp/src/Models/Location.cs
@@ -1,9 +1,25 @@
-using System;
+using System.Text.Json.Serialization;
 using static JavaGameButCSharp.OptionMap;
 
 
 namespace JavaGameButCSharp{
     class Location : StatefulObject{
+        [JsonPropertyName("name")]
+        public string Name {get; set;}
+
+        [JsonPropertyName("description")]
+        public string Description {get; set;}
+
+        [JsonPropertyName("nextLocation")]
+        public string NextLocation {get; set;}
+
+        [JsonPropertyName("previousLocation")]
+        public string PreviousLocation {get; set;}
+
+        [JsonPropertyName("npcList")]   
+        public List<String> NpcLis {get; set;}     
+        
+        
         public Location(string fileName) : base(fileName, LOCATION){
         }
 
