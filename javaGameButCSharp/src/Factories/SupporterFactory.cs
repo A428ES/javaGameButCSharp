905124d42f96d02b6a25bd35f7855a7f66ebc7cb
sergio estrada <estrada.serg@gmail.com>
2024-11-09 15:10:03 -0600

    added additional routes

--- javaGameButCSharp/src/Factories/SupporterFactory.cs
+++ javaGameButCSharp/src/Factories/SupporterFactory.cs
@@ -2,12 +2,13 @@ using static JavaGameButCSharp.OptionMap;
 
 namespace JavaGameButCSharp{
     class SupporterFactory{
-        public static ISupporter GenerateSupporter(OptionMap type, SupporterContext supportContext){
+        public static Supporter GenerateSupporter(OptionMap type, SupporterContext supportContext){
             return type switch{
-                ITEM_EVENT => new ItemSupporter(supportContext),
+                INVENTORY => new InventorySupporter(supportContext),
                 MENU_EVENT => new MainMenuEventSupporter(supportContext),
-                ENTITY_EVENT => new BattleSupporter(supportContext),
+                ENTITY_EVENT => new EntitySupporter(supportContext),
                 LOCATION_EVENT => new LocationSupporter(supportContext),
+                BATTLE_EVENT => new BattleSupporter(supportContext),
                 _=> throw new Exception("No options")
             };
         }
