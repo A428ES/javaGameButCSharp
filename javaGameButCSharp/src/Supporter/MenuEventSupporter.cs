f855c015b95b9f731032e42d646b0d853db74355
sergio estrada <estrada.serg@gmail.com>
2024-11-09 07:43:02 -0600

    major changes to match new retry standard

--- javaGameButCSharp/src/Supporter/MenuEventSupporter.cs
+++ javaGameButCSharp/src/Supporter/MenuEventSupporter.cs
@@ -1,20 +1,15 @@
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic.FileIO;
 using static JavaGameButCSharp.OptionMap;
 
 namespace JavaGameButCSharp{
     class MainMenuEventSupporter(SupporterContext supporterContext) : ISupporter{
         private readonly SupporterContext _supporterContext = supporterContext;
-        public void NewGame(OptionMap enumResult){
+        public void NewGame(){
             _supporterContext.IO.OutWithPrompt("CREATING NEW SAVE", "ENTER YOUR NAME");
             
-            try{
-                _supporterContext.GameState.NewPlayer(_supporterContext.IO.LastUserInput);
-                LoadPlayerLocation();
-            } catch (InvalidInput)
-            {
-                _supporterContext.IO.OutWithSubject("ERROR", "This save already exists!");
-
-                Routing(enumResult);
-            }
+            _supporterContext.GameState.NewPlayer(_supporterContext.IO.LastUserInput);
+            LoadPlayerLocation();
         }
 
         public void Stats(){
@@ -30,6 +25,7 @@ namespace JavaGameButCSharp{
         public void LoadGame(){
             _supporterContext.IO.OutWithPrompt("LOADING EXISTING GAME", "ENTER YOUR SAVE NAME");
             _supporterContext.GameState.LoadPlayer(_supporterContext.IO.LastUserInput);
+
             LoadPlayerLocation();
         }
 
@@ -38,34 +34,71 @@ namespace JavaGameButCSharp{
             _supporterContext.SystemEvent = new EventModel(EXIT);
         }
         
-        public void Routing(OptionMap overrideInput = EVENT_COMPLETE) {
-            EventModel nextEvent;
-            OptionMap enumResult = overrideInput;
+        public void Resume(){
+            _supporterContext.ResumeGame();
+        }
 
-            if(overrideInput == EVENT_COMPLETE){
-                _supporterContext.IO.OutWithOptionsPrompt(_supporterContext.WorkingEvent.EventText, _supporterContext.WorkingEvent.InputOptions);
+        public void Delete(){
+            _supporterContext.IO.OutWithPrompt("DELETING GAME SAVE","ENTER SAVE TO DELETE");
 
-                if(!Enum.TryParse<OptionMap>(_supporterContext.IO.LastUserInput, true, out enumResult)){
-                    throw new InvalidInput("Not a valid game engine input");
-                }
+            string toDelete = _supporterContext.IO.LastUserInput;
+
+            if(!_supporterContext.GameState.GetGameSaves().Contains(toDelete)){
+                _supporterContext.IO.OutWithSubject("ERROR", "SAVE DOES NOT EXIST");
+                return;
             }
+
+            _supporterContext.IO.OutWithPrompt("ARE YOU SURE?",  "ENTER YES TO CONFIRM");
+            
+            if(_supporterContext.IO.LastUserInput.ToUpper().Equals("YES")){
+                _supporterContext.GameState.DeleteSave(toDelete);
+            }
+        }
+
+        public List<string> GetAllUserOptions(){
+            var options = new List<string>(_supporterContext.WorkingEvent.InputOptions);
+
+            if (!string.IsNullOrEmpty(_supporterContext.GameState.ActivePlayer.Name) && !options.Contains("RESUME")){
+                options.Add("RESUME");
+            }
+
+            return options;
+        }
+
+        public void RetryWorker(Action action){
+            _supporterContext.RetryHelper.ExecuteWithRetry(action, 5);
+        }
+
+        public void Routing() {
+            OptionMap enumResult;
+            
+            _supporterContext.IO.OutWithOptionsPrompt(_supporterContext.WorkingEvent.EventText, GetAllUserOptions());
+            Enum.TryParse<OptionMap>(_supporterContext.IO.LastUserInput, true, out enumResult);
 
             switch(enumResult){
                 case EXIT:
                     ExitGame();
-                break;
+                    break;
                 case NEW:
-                    NewGame(enumResult);
-                break;
+                    _supporterContext.RetryHelper.ExecuteWithRetry(()=> NewGame(), 5);
+                    break;
                 case LOAD:
-                    LoadGame();
-                break;
+                    _supporterContext.RetryHelper.ExecuteWithRetry(()=> LoadGame(), 5);
+                    break;
                 case STATS:
                     Stats();
-                break;
+                    break;
+                case RESUME:
+                    Resume();
+                    break;
+                case DELETE:
+                    _supporterContext.RetryHelper.ExecuteWithRetry(()=> Delete(), 5);
+                    break;
                 default:
-                    nextEvent = new EventModel(LOCATION_EVENT, "tatooine");
-                break;
+                    _supporterContext.IO.OutWithSubject("ERROR", "Invalid entry");
+
+                    Routing();
+                    break;
             }
         }
     }
