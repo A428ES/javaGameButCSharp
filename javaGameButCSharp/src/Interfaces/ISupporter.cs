5fac789be8357b002b441352925082de212cc3b0
sergio estrada <estrada.serg@gmail.com>
2024-11-09 07:41:34 -0600

    removed optionmap param requirement

--- javaGameButCSharp/src/Interfaces/ISupporter.cs
+++ javaGameButCSharp/src/Interfaces/ISupporter.cs
@@ -2,6 +2,6 @@ using static JavaGameButCSharp.OptionMap;
 
 namespace JavaGameButCSharp{
     interface ISupporter{
-        public void Routing(OptionMap overrideInput = EVENT_COMPLETE);
+        public void Routing();
     }
 }
