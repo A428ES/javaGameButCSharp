933b531d43bcfbde76e9137f009841e03c73a50d
sergio estrada <estrada.serg@gmail.com>
2024-11-10 10:56:19 -0600

    added item event

--- javaGameButCSharp/src/Factories/SupporterFactory.cs
+++ javaGameButCSharp/src/Factories/SupporterFactory.cs
@@ -4,11 +4,12 @@ namespace JavaGameButCSharp{
     class SupporterFactory{
         public static Supporter GenerateSupporter(OptionMap type, SupporterContext supportContext){
             return type switch{
-                INVENTORY => new InventorySupporter(supportContext),
+                INVENTORY_EVENT => new InventorySupporter(supportContext),
                 MENU_EVENT => new MainMenuEventSupporter(supportContext),
                 ENTITY_EVENT => new EntitySupporter(supportContext),
                 LOCATION_EVENT => new LocationSupporter(supportContext),
                 BATTLE_EVENT => new BattleSupporter(supportContext),
+                ITEM_EVENT => new ItemSupporter(supportContext),
                 _=> throw new Exception("No options")
             };
         }
