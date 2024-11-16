2e419a244bedb74aa0c522c3a1a7ff259a3b9ace
sergio estrada <estrada.serg@gmail.com>
2024-11-16 08:00:20 -0600

    added boundary to display context class

--- javaGameButCSharp/src/Display/DisplayContext.cs
+++ javaGameButCSharp/src/Display/DisplayContext.cs
@@ -4,10 +4,12 @@ namespace JavaGameButCSharp{
     public class DisplayContext{
         public EngineMenu GameMenu {get;}
         public Canvas GameCanvas {get;}
+        public Boundary Boundary {get;}
  
-        public DisplayContext(EngineMenu gameMenu, Canvas gameCanvas){
+        public DisplayContext(EngineMenu gameMenu, Canvas gameCanvas, Boundary boundary){
             GameCanvas = gameCanvas;
             GameMenu = gameMenu;
+            Boundary = boundary;
         }
 
     }
