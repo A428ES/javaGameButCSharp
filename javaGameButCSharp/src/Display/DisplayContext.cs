f769e2cefb5dd03b7d02b15ad974297ae05cc86d
sergio estrada <estrada.serg@gmail.com>
2024-11-17 15:18:15 -0600

    changed displaycontext responisibilities

--- javaGameButCSharp/src/Display/DisplayContext.cs
+++ javaGameButCSharp/src/Display/DisplayContext.cs
@@ -1,16 +1,31 @@
-using System.Windows.Controls;
-
 namespace JavaGameButCSharp{
-    public class DisplayContext{
-        public EngineMenu GameMenu {get;}
-        public Canvas GameCanvas {get;}
-        public Boundary Boundary {get;}
- 
-        public DisplayContext(EngineMenu gameMenu, Canvas gameCanvas, Boundary boundary){
-            GameCanvas = gameCanvas;
-            GameMenu = gameMenu;
-            Boundary = boundary;
+    class DisplayContext(GameRenderer gameRenderer)
+    {
+        private Sprite _playerSprite;
+        private List<Sprite> _npcSprites;
+        public GameRenderer Renderer { get; } = gameRenderer;
+
+        public void SpawnPlayer(){
+            Renderer.RenderPlayerSprite(@"C:\Walk.PNG");
+            _npcSprites = new();
         }
 
+        public void LoadSprite(Sprite newSprite){
+            _npcSprites.Add(newSprite);
+        }
+
+        public bool PlayerLoaded(){
+            return _playerSprite != null;
+        }
+
+        public void DestroyPlayer(){
+            if(!PlayerLoaded()){
+                Renderer.DestroySprite(_playerSprite);
+            }
+        }
+
+        public void UpdateMenu(Dictionary<OptionMap, Action> menuMap){
+            Renderer.GameMenu.RefreshMenuStack("MENU", menuMap);
+        }
     }
 }
