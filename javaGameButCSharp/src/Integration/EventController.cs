bfeafa49d0377e0be9b36c838d4063cb65cb2ddd
sergio estrada <estrada.serg@gmail.com>
2024-11-17 15:19:25 -0600

    removed render logic

--- javaGameButCSharp/src/Integration/EventController.cs
+++ javaGameButCSharp/src/Integration/EventController.cs
@@ -10,13 +10,11 @@ namespace JavaGameButCSharp{
         private readonly InputOutManager _IO;
         private readonly GameStateController _gameState;
         private readonly DisplayContext _displayContext;
-        private Sprite sprite {get;set;}
-    
         public EventController(StateManagement stateManagement, SaveLoadManagement saveLoad, GameStateController gameState, DisplayContext displayContext){
             CurrentEvent = new(MENU_EVENT);
             LastEvent = EventModel.Copy(CurrentEvent);
             _displayContext = displayContext;
-            displayContext.GameMenu.converter = GUIMenu_Injection;
+            displayContext.Renderer.GameMenu.converter = GUIMenu_Injection;
 
             _IO = new InputOutManager();
             _gameState = gameState;
@@ -25,47 +23,41 @@ namespace JavaGameButCSharp{
 
             _eventSupporter = new EventSupporter(stateManagement, saveLoad, supporterContext);
         }
-
+        
         public void GUIMenu_Injection(object sender, RoutedEventArgs e)
         {
-            if (sender is MenuItem menuItem)
-            {
-                if (menuItem.Tag is OptionMap option)
-                {
-                    CurrentEvent = new EventModel(MENU_EVENT);
-                    _IO.LastUserInput = option.ToString();
-                    
-                    RunNextEvent();
-                }
-                else
-                {
-                    System.Windows.MessageBox.Show("Invalid option type in MenuItem tag.");
+            if (sender is MenuItem menuItem) {
+                if (menuItem.Tag is OptionMap option) {
+                    RouteMenuEvent(option.ToString());
+                } else {
+                    _IO.CentralErrorOutput("Invalid option type in MenuItem tag.");
                 }
             }
         }
 
-        public void ClearCanvas(){}
+        public void RouteMenuEvent(string inputInject){
+            if (string.IsNullOrEmpty(inputInject)) {
+                _IO.CentralErrorOutput("Invalid option type in MenuItem tag.");
+                return;
+            }
+            
+            CurrentEvent = new EventModel(MENU_EVENT);
+            _IO.LastUserInput = inputInject;
+                    
+            RunNextEvent();
+        }
+
         public void RunNextEvent(){
             Supporter supporterObject = _eventSupporter.RunEvent(CurrentEvent);
+            _displayContext.UpdateMenu(supporterObject.MapRoute());
 
             LastEvent.EventOutCome = _eventSupporter.EventOutCome;
             CurrentEvent = _eventSupporter.NextEvent;
 
-            _displayContext.GameMenu.LoadMenuStack("MENU", supporterObject.MapRoute());
-
-            if(!_gameState.ActivePlayer.Name.Equals("") && sprite == null){
-                sprite = new Sprite(
-                    @"C:\Walk.png", 
-                    new System.Windows.Point(100, 100), 
-                    _displayContext.GameCanvas, 
-                    _displayContext.Boundary,
-                    frameCount: 6, 
-                    frameWidth: 42, 
-                    frameHeight: 42
-                );
-            } else if(_gameState.ActivePlayer.Name.Equals("") && sprite != null){
-                sprite.DestroySprite();
-                sprite = null;
+            if(!_gameState.ActivePlayer.Name.Equals("") && !_displayContext.PlayerLoaded()){
+                _displayContext.SpawnPlayer();
+            } else if(_displayContext.PlayerLoaded()) {
+                _displayContext.DestroyPlayer();
             }
         }
 
